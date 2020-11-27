d2cac08aecf9c4ea5fdf2df8db110ee973d6604a
Álvaro SF <alvaro@onclic.es>
2020-11-27 07:34:02 +0100

    Testing the Product Controller

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class ProductController extends AbstractController
+{
+     /**
+     * @Route("/product", name="product")
+     */
+    public function index(): Response
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+    
+    /**
+     * @Route("/product/new", name="create_product")
+     */
+    public function createProduct(): Response
+    {
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+        $product->setName('Table');
+        // $product->setPrice(1999);
+        $product->setDescription('A test description');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+    
+   
+}
