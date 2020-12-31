f9701ba408668b48fd514d821f75a212a160a403
Joaquín Cuéllar <joa.cuellar@riseup.net>
2020-12-31 14:38:20 +0100

    create basic product table

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -2,57 +2,336 @@
 
 namespace App\Entity;
 
-use App\Repository\ProductRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=ProductRepository::class)
+ * Product
+ *
+ * @ORM\Table(name="sumak_product", indexes={@ORM\Index(name="fk_id_parent", columns={"id_parent"})})
+ * @ORM\Entity
  */
 class Product
 {
     /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var int
+     *
+     * @ORM\Column(name="id_default_category", type="integer", nullable=false)
      */
-    private $name;
+    private $idDefaultCategory;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @var int
+     *
+     * @ORM\Column(name="id_seller", type="integer", nullable=false)
      */
-    private $description;
+    private $idSeller;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=10, nullable=false)
+     */
+    private $type;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="reference", type="string", length=100, nullable=true)
+     */
+    private $reference;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="ean13", type="string", length=100, nullable=true)
+     */
+    private $ean13;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="on_sale", type="integer", nullable=false)
+     */
+    private $onSale;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="active", type="integer", nullable=false)
+     */
+    private $active;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_tax_group", type="integer", nullable=false)
+     */
+    private $idTaxGroup;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="quantity", type="integer", nullable=false)
+     */
+    private $quantity = '0';
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="low_stock_alert", type="integer", nullable=false, options={"default"="1"})
+     */
+    private $lowStockAlert = '1';
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="min_quantity", type="integer", nullable=false, options={"default"="1"})
+     */
+    private $minQuantity = '1';
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="weight", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $weight;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="width", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $width;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="height", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $height;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="depth", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $depth;
+
+    /**
+     * @var \Product
+     *
+     * @ORM\ManyToOne(targetEntity="Product")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_parent", referencedColumnName="id")
+     * })
+     */
+    private $idParent;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getIdDefaultCategory(): ?int
     {
-        return $this->name;
+        return $this->idDefaultCategory;
     }
 
-    public function setName(string $name): self
+    public function setIdDefaultCategory(int $idDefaultCategory): self
     {
-        $this->name = $name;
+        $this->idDefaultCategory = $idDefaultCategory;
 
         return $this;
     }
 
-    public function getDescription(): ?string
+    public function getIdSeller(): ?int
     {
-        return $this->description;
+        return $this->idSeller;
     }
 
-    public function setDescription(?string $description): self
+    public function setIdSeller(int $idSeller): self
     {
-        $this->description = $description;
+        $this->idSeller = $idSeller;
 
         return $this;
     }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(?string $reference): self
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getEan13(): ?string
+    {
+        return $this->ean13;
+    }
+
+    public function setEan13(?string $ean13): self
+    {
+        $this->ean13 = $ean13;
+
+        return $this;
+    }
+
+    public function getOnSale(): ?int
+    {
+        return $this->onSale;
+    }
+
+    public function setOnSale(int $onSale): self
+    {
+        $this->onSale = $onSale;
+
+        return $this;
+    }
+
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
+
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+
+    public function getIdTaxGroup(): ?int
+    {
+        return $this->idTaxGroup;
+    }
+
+    public function setIdTaxGroup(int $idTaxGroup): self
+    {
+        $this->idTaxGroup = $idTaxGroup;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getLowStockAlert(): ?int
+    {
+        return $this->lowStockAlert;
+    }
+
+    public function setLowStockAlert(int $lowStockAlert): self
+    {
+        $this->lowStockAlert = $lowStockAlert;
+
+        return $this;
+    }
+
+    public function getMinQuantity(): ?int
+    {
+        return $this->minQuantity;
+    }
+
+    public function setMinQuantity(int $minQuantity): self
+    {
+        $this->minQuantity = $minQuantity;
+
+        return $this;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(?float $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getWidth(): ?float
+    {
+        return $this->width;
+    }
+
+    public function setWidth(?float $width): self
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    public function getHeight(): ?float
+    {
+        return $this->height;
+    }
+
+    public function setHeight(?float $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getDepth(): ?float
+    {
+        return $this->depth;
+    }
+
+    public function setDepth(?float $depth): self
+    {
+        $this->depth = $depth;
+
+        return $this;
+    }
+
+    public function getIdParent(): ?self
+    {
+        return $this->idParent;
+    }
+
+    public function setIdParent(?self $idParent): self
+    {
+        $this->idParent = $idParent;
+
+        return $this;
+    }
+
+
 }
